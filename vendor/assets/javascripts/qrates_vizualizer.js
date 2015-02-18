9150c3db7efaadc9f9f9bbe46fa59a70c2f44dd1
nulltask <nulltask@gmail.com>
2015-02-18 16:28:22 +0900

    assets 追加

---
+++ vendor/assets/javascripts/qrates_vizualizer.js
@@ -0,0 +1,23 @@
+
+//= require tdmsinc-three.js
+//= require_tree ./qrates_visualizer
+
+(function(global, exports) {
+
+  /**
+   * Expose `VinylVisualizer`.
+   */
+
+  exports.VinylVisualizer = VinylVisualizer;
+
+  /**
+   * Constructor.
+   *
+   * @api public
+   */
+
+  function VinylVisualizer() {
+    // TODO:
+  }
+
+})(this, (this.qvv = (this.qvv || {})));
