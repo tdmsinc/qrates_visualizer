cc470dbafcebb590318e030d683beca9d4490d54
nulltask <nulltask@gmail.com>
2015-02-18 19:14:17 +0900

    定数一覧を追加 (色はまだ)

---
+++ vendor/assets/javascripts/qrates_visualizer/enum.js
@@ -0,0 +1,78 @@
+
+(function(global, exports) {
+
+  /**
+   * Expose `VINYL_TYPE`.
+   */
+
+  exports.VINYL_TYPE = {};
+  exports.VINYL_TYPE.BLACK = 1;
+  exports.VINYL_TYPE.COLOR = 2;
+  exports.VINYL_TYPE.SPLATTER = 3;
+
+  /**
+   * Expose `VINYL_SIZE`.
+   */
+
+  exports.VINYL_SIZE = {};
+  exports.VINYL_SIZE.SEVEN = 7;
+  exports.VINYL_SIZE.TEN = 10;
+  exports.VINYL_SIZE.TWELVE = 12;
+
+  /**
+   * Expose `VINYL_COLOR`
+   */
+
+  exports.VINYL_COLOR = {};
+  // TODO: add vinyl color.
+
+  /**
+   * Expose `VINYL_BASE_COLOR`.
+   */
+
+  exports.VINYL_BASE_COLOR = {};
+  // TODO: add vinyl base color.
+
+  /**
+   * Expose `VINYL_TRANSPARENT_COLOR`.
+   */
+
+  exports.VINYL_TRANSPARENT_COLOR = {};
+  // TODO: add vinyl transparent color.
+
+  /**
+   * Expose `VINYL_HOLE_SIZE`.
+   */
+
+  exports.VINYL_HOLE_SIZE = {};
+  exports.VINYL_HOLE_SIZE.SMALL = 0;
+  exports.VINYL_HOLE_SIZE.BIG = 1;
+
+  /**
+   * Expose `VINYL_RPM`.
+   */
+
+  exports.VINYL_RPM = {};
+  exports.VINYL_RPM.THIRTYTHREE = 33;
+  exports.VINYL_RPM.FOURTYFIVE = 45;
+
+  /**
+   * Expose `LABEL_TYPE`.
+   */
+
+  exports.LABEL_TYPE = {};
+  exports.LABEL_TYPE.WHITE = 1;
+  exports.LABEL_TYPE.MONOCHROME = 2;
+  exports.LABEL_TYPE.COLOR = 3;
+
+  /**
+   * Expose `SLEEVE_TYPE`.
+   */
+
+  exports.SLEEVE_TYPE = {};
+  exports.SLEEVE_TYPE.BLACK = 1;
+  exports.SLEEVE_TYPE.WHITE = 2;
+  exports.SLEEVE_TYPE.PRINT = 3;
+  exports.SLEEVE_TYPE.PRINT_WITH_SPINE = 4;
+
+})(this, (this.qvv = (this.qvv || {})));
