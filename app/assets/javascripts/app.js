29e0b6d5dc3f5e970d0add9156d9840b230a632e
nulltask <nulltask@gmail.com>
2015-02-20 13:26:03 +0900

    テスト用インタフェース追加

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,6 +6,50 @@
 var el = document.querySelector('.vinyl-visualizer-container');
 var vv = new qvv.VinylVisualizer(el);
 
+// initialize view model.
+var defaults = {
+  visualizer: {
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
+  vinyl: {
+    type: 1,
+    size: 7,
+    holeSize: 0,
+    heavy: false,
+    speed: 45,
+  },
+  label: {
+    type: 1
+  },
+  sleeve: {
+    type: 1,
+    hole: false,
+    glossFinished: false
+  }
+};
+var vm = new Vue({ el: 'form', data: defaults });
+
+vm.$watch('visualizer.width', function(value) {
+  vv.resize(value, vm.$get('visualizer.height'));
+});
+vm.$watch('visualizer.height', function(value) {
+  vv.resize(vm.$get('visualizer.width'), value);
+});
+vm.$watch('visualizer.view', function(value) {
+  vv.view(value, { transition: vm.$get('visualizer.transition') });
+});
+
+vm.$watch('vinyl.type', function(value) { vv.vinyl.type(value); });
+vm.$watch('vinyl.size', function(value) { vv.vinyl.size(value); });
+vm.$watch('vinyl.holeSize', function(value) { vv.vinyl.holeSize(value); });
+vm.$watch('vinyl.heavy', function(value) { vv.vinyl.heavy(value); });
+vm.$watch('vinyl.speed', function(value) { vv.vinyl.speed(value); });
+vm.$watch('label.type', function(value) { vv.label.type(value); });
+vm.$watch('sleeve.type', function(value) { vv.sleeve.type(value); });
+vm.$watch('sleeve.hole', function(value) { vv.sleeve.hole(value); });
+vm.$watch('sleeve.glossFinished', function(value) { vv.sleeve.glossFinished(value); });
+
 // ready to use vinyl visualizer
 vv.on('ready', function() {
   console.log('visualizer is ready.');
