c06c88e6882049caa169caeb15b624af16068803
nulltask <nulltask@gmail.com>
2015-02-20 17:49:10 +0900

    ファイルの変更を監視可能に
    - Vue.js が input[type=file] の変更を検出できなかったため標準仕様で書き直し

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,10 @@
 //= require qrates_visualizer
 //= require_self
 
+//
 // defaults
+//
+
 var defaults = {
   visualizer: {
     width: window.innerWidth,
@@ -25,7 +28,10 @@ var defaults = {
   }
 };
 
+//
 // initialize vinyl visualizer.
+//
+
 var el = document.querySelector('.vinyl-visualizer-container');
 var vv = new qvv.VinylVisualizer(el, {
   defaults: {
@@ -35,46 +41,124 @@ var vv = new qvv.VinylVisualizer(el, {
   }
 });
 
+//
 // initialize view model.
+//
+
 var vm = new Vue({ el: 'form', data: defaults });
 
-vm.$watch('visualizer.width', function(value) {
-  vv.resize(value, vm.$get('visualizer.height'));
-});
-vm.$watch('visualizer.height', function(value) {
-  vv.resize(vm.$get('visualizer.width'), value);
-});
-vm.$watch('visualizer.view', function(value) {
-  vv.view(value, { transition: vm.$get('visualizer.transition') });
-});
-
-vm.$watch('vinyl.type', function(value) { vv.vinyl.type(value); });
-vm.$watch('vinyl.size', function(value) { vv.vinyl.size(value); });
-vm.$watch('vinyl.holeSize', function(value) { vv.vinyl.holeSize(value); });
-vm.$watch('vinyl.heavy', function(value) { vv.vinyl.heavy(value); });
-vm.$watch('vinyl.speed', function(value) { vv.vinyl.speed(value); });
-vm.$watch('label.type', function(value) { vv.label.type(value); });
-vm.$watch('sleeve.type', function(value) { vv.sleeve.type(value); });
-vm.$watch('sleeve.hole', function(value) { vv.sleeve.hole(value); });
-vm.$watch('sleeve.glossFinished', function(value) { vv.sleeve.glossFinished(value); });
-
+//
 // ready to use vinyl visualizer
+//
+
 vv.on('ready', function() {
   console.log('visualizer is ready.');
-  // setters.
-  vv.vinyl.type(1).size(12);
-  vv.label.type(1);
-  vv.sleeve.glossFinished(true);
-  setTimeout(function() {
-    // getters.
-    console.log('vv.vinyl.type() returns', vv.vinyl.type());
-    console.log('vv.vinyl.size() returns', vv.vinyl.size());
-    console.log('vv.label.type() returns', vv.label.type());
-    console.log('vv.sleeve.glossFinished() returns', vv.sleeve.glossFinished());
-  }, 1000);
+
+  //
+  // delegate events
+  //
+
+  vm.$watch('visualizer.width', function(value) {
+    vv.resize(value, vm.$get('visualizer.height'));
+  });
+  vm.$watch('visualizer.height', function(value) {
+    vv.resize(vm.$get('visualizer.width'), value);
+  });
+  vm.$watch('visualizer.view', function(value) {
+    vv.view(value, { transition: vm.$get('visualizer.transition') });
+  });
+
+  vm.$watch('vinyl.type', function(value) { vv.vinyl.type(value); });
+  vm.$watch('vinyl.size', function(value) { vv.vinyl.size(value); });
+  vm.$watch('vinyl.holeSize', function(value) { vv.vinyl.holeSize(value); });
+  vm.$watch('vinyl.heavy', function(value) { vv.vinyl.heavy(value); });
+  vm.$watch('vinyl.speed', function(value) { vv.vinyl.speed(value); });
+  vm.$watch('label.type', function(value) { vv.label.type(value); });
+  vm.$watch('sleeve.type', function(value) { vv.sleeve.type(value); });
+  vm.$watch('sleeve.hole', function(value) { vv.sleeve.hole(value); });
+  vm.$watch('sleeve.glossFinished', function(value) { vv.sleeve.glossFinished(value); });
+
+  //
+  // file watcher
+  //
+
+  var vinylA = document.querySelector('input[name=vinyl-a]');
+  var vinylB = document.querySelector('input[name=vinyl-b]');
+  var labelA = document.querySelector('input[name=label-a]');
+  var labelB = document.querySelector('input[name=label-b]');
+  var sleeveFront = document.querySelector('input[name=sleeve-front]');
+  var sleeveBack = document.querySelector('input[name=sleeve-back]');
+  var sleeveSpine = document.querySelector('input[name=sleeve-spine]');
+
+  /**
+   * @param {File} file
+   * @param {Function} callback
+   */
+
+  function load(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var img = new Image();
+      img.onerror =
+      img.onabort = function(e) {
+        callback(e);
+      };
+      img.onload = function(e) {
+        callback(null, e.target);
+      };
+      img.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  vinylA.onchange = function(e) {
+    load(e.target.files.item(0), function(err, img) {
+      if (err) return console.error(err);
+      vv.vinyl.sideATexture(img);
+    });
+  };
+  vinylB.onchange = function(e) {
+    load(e.target.files.item(0), function(err, img) {
+      if (err) return console.error(err);
+      vv.vinyl.sideBTexture(img);
+    });
+  };
+  labelA.onchange = function(e) {
+    load(e.target.files.item(0), function(err, img) {
+      if (err) return console.error(err);
+      vv.label.sideATexture(img);
+    });
+  };
+  labelB.onchange = function(e) {
+    load(e.target.files.item(0), function(err, img) {
+      if (err) return console.error(err);
+      vv.label.sideBTexture(img);
+    });
+  };
+  sleeveFront.onchange = function(e) {
+    load(e.target.files.item(0), function(err, img) {
+      if (err) return console.error(err);
+      vv.sleeve.frontTexture(img);
+    });
+  };
+  sleeveBack.onchange = function(e) {
+    load(e.target.files.item(0), function(err, img) {
+      if (err) return console.error(err);
+      vv.sleeve.backTexture(img);
+    });
+  };
+  sleeveSpine.onchange = function(e) {
+    load(e.target.files.item(0), function(err, img) {
+      if (err) return console.error(err);
+      vv.sleeve.spineTexture(img);
+    });
+  };
 });
 
+//
 // observers. (for internal use)
+//
+
 vv.vinyl.on('type', function(value) {
   console.log('change vinyl.type', value);
 });
