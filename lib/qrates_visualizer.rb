bb738284c17853dbfdc9b263eaccce6e6af69c03
Oleksii Vykhor <o.vykhor@ukr.net>
2020-09-11 12:59:46 +0300

    manifest.js

--- lib/qrates_visualizer.rb
+++ lib/qrates_visualizer.rb
@@ -4,6 +4,9 @@ module QratesVisualizer
   module Rails
     if defined?(::Rails)
       class Engine < ::Rails::Engine
+        initializer 'qrates_visualizer.assets.precompile' do |app|
+          app.config.assets.precompile << 'config/qrates_visualizer_manifest.js'
+        end
       end
     end
   end
